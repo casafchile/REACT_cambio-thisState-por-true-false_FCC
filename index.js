4e02221daebe9f76b5ce3562bc48ccfb5205eddd
Felipe Herrera <83690907+casafchile@users.noreply.github.com>
2024-07-04 17:27:00 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,38 @@
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      visibility: false
+    };
+    // change code below this line
+    this.toggleVisibility = this.toggleVisibility.bind(this);      //determina el tipo
+    // change code above this line
+  }
+  // change code below this line                                   //if y else de determinar el tur y el false
+  toggleVisibility() {
+    this.setState(state => {
+      if (state.visibility === true) {
+         return { visibility: false };
+       } else {
+         return { visibility: true };
+      }
+    });
+  }
+  // change code above this line
+  render() {
+    if (this.state.visibility) {
+      return (
+        <div>
+          <button onClick={this.toggleVisibility}>Click Me</button>      //cambio al presionar
+          <h1>Now you see me!</h1>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <button onClick={this.toggleVisibility}>Click Me</button>
+        </div>
+      );
+    }
+  }
+};
